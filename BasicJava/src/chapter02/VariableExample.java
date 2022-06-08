aff70e07d5955012bc9fdb547836fb0b2d37d593
anhy111 <gkdyd123@gmail.com>
2022-06-08 10:38:45 +0900

    이클립스 연동 후 첫 push

---
+++ BasicJava/src/chapter02/VariableExample.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int value = 0;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+		
+	}
+		
+		
+
+	
+
+}
---
+++ BasicJava/src/chapter02/VariableExchangeExample.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class VariableExchangeExample {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		System.out.println("x: " + x + ", y: " + y);
+		
+		int z = x;
+		x = y;
+		y = z;
+		
+		System.out.println("x: " + x + ", y: " + y);
+		
+	}
+}
