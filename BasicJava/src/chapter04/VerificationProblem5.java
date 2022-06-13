dc1227671c88db9d2c88425069f439e431723d63
anhy111 <67471703+anhy111@users.noreply.github.com>
2022-06-14 00:41:16 +0900

    Update VerificationProblem5.java
    가독성 수정

--- BasicJava/src/chapter04/VerificationProblem5.java
+++ BasicJava/src/chapter04/VerificationProblem5.java
@@ -3,8 +3,8 @@ package chapter04;
 public class VerificationProblem5 {
 	public static void main(String[] args) {
 		
-		for(int j=1; j<=4;j++) {
-			for(int i=1; i<=j; i++) {
+		for(int j=0; j<4;j++) {
+			for(int i=0; i<j+1; i++) {
 				System.out.print('*');
 			}
 			System.out.println("\n");
