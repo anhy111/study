57328b3f48e5af385e7ca545a59eb0edc8e7d8e4
anhy111 <gkdyd123@gmail.com>
2022-06-25 17:49:59 +0900

    개인공부

---
+++ BasicJava/src/jungsuk06/Exercise6_23.java
@@ -0,0 +1,24 @@
+package jungsuk06;
+
+public class Exercise6_23 {
+	public static void main(String[] args) {
+		int[] data = {3,2,9,4,7};
+		System.out.println(java.util.Arrays.toString(data));
+		System.out.println("최대값:"+max(data));
+		System.out.println("최대값:"+max(null));
+		System.out.println("최대값:"+max(new int[] {}));
+	}
+	
+	static int max(int[] arr) {
+		if(arr == null || arr.length == 0) {
+			return -999999;
+		}
+		int maxNum = arr[0];
+		for(int i=1; i<arr.length; i++) {
+			if(maxNum < arr[i]) {
+				maxNum = arr[i];
+			}
+		}
+		return maxNum;
+	}
+}
